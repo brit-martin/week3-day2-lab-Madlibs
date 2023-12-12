ee27e1201983629c7b6f357cb8210e7e3f84751d
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2023-12-12 16:04:57 -0700

    lab finished just not the further study

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import nunjucks from 'nunjucks';
+import sample from 'lodash.sample';
 
 const app = express();
 const port = '8000';
-
+//middle ware------------------------------------
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -12,7 +13,7 @@ nunjucks.configure('views', {
   autoescape: true,
   express: app,
 });
-
+//--------------------------------------------
 // Run the server.
 const server = app.listen(port, () => {
   console.log(`Server running on http://localhost:${server.address().port}...`);
@@ -36,8 +37,8 @@ const COMPLIMENTS = [
 ];
 
 // Display the homepage
-app.get('/', (req, res) => {
-  res.render('index.html');
+app.get('/', (request, response) => {
+  response.render('index.html');
 });
 
 // Display a form that asks for the user's name.
@@ -48,5 +49,32 @@ app.get('/hello', (req, res) => {
 // Handle the form from /hello and greet the user.
 app.get('/greet', (req, res) => {
   const name = req.query.name || 'stranger';
-  res.render('greet.html.njk', { name: name });
+  const compliment = sample(COMPLIMENTS);
+  res.render('greet.html.njk', {
+    name: name,
+    compliment: compliment
+    });
 });
+
+app.get('/game', (req, res) => {
+  // console.log("/game")
+  if (req.query.playgame === "no"){
+    res.render('goodbye.html.njk')
+  } else if 
+    (req.query.playgame === "yes"){
+      res.render('game.html.njk')
+  }
+  
+});
+
+app.get('/madlib', (req, res) => {
+  // console.log(req.query)
+const {color, noun, person, adjective} = req.query
+  res.render('madlib.html.njk', {
+    color: color,
+    noun: noun,
+    person: person,
+    adjective: adjective
+  });
+
+})
